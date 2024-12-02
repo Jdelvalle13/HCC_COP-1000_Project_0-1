4352f58c7eb9eaedcc699cbf6444e9308b4f495b
Jaime Del Valle <jdelvalle13@hawkmail.hccfl.edu>
2024-12-01 21:10:34 -0500

    updated reset function

--- CarFinder_1.1.py
+++ CarFinder_1.1.py
@@ -1,6 +1,6 @@
 # Declarations
-allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
-menuOptions = ['PRINT all Authorized Vehicles', 'SEARCH for Authorized Vehicle', 'ADD Authorized Vehicle', 'DELETE Authorized Vehicle']
+allowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500']
+menuOptions = ['PRINT all Authorized Vehicles', 'SEARCH for Authorized Vehicle', 'ADD Authorized Vehicle', 'DELETE Authorized Vehicle', 'RESET/RESTORE Authorized Vehicle List']
 authorizedVehicles = "Authorized_Vehicle_File.txt"
 
 def display_menu(menuOptions):
@@ -10,7 +10,7 @@ def display_menu(menuOptions):
     
     print()
     print('********************************')
-    print('AutoCountry Vehicle Finder v0.2')
+    print('AutoCountry Vehicle Finder v1.0')
     print("********************************")
     print()
     print('Please Enter the following number below from the following menu:')
@@ -34,29 +34,30 @@ def create_authorized_vehicles_file (authorizedVehicles):
         with open(authorizedVehicles, 'w') as db:
             for vehicle in allowedVehiclesList:
                 db.write(vehicle + "\n")
+            
+            # Close File
+            db.close()
 
 def print_authorized_vehicles(authorizedVehicles):
     
     ## Print all authorized vehicle in file
-    ## If fil
-    
     import os
-    if os.path.isfile(authorizedVehicles):
-        with open(authorizedVehicles, 'r') as db:
-            vehicles = db.readlines()
-    
-            #Strip away extra whitespace 
-            vehicles = [vehicle.strip() for vehicle in vehicles]
-    else:
-        vehicles = []
-    
-    # Inform the user of authorized vehicles
-    print('    The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
-    print()
-    
-    # Print each vehicle on a separate line
-    for vehicle in vehicles:
-        print(f'      {vehicle}')
+    with open(authorizedVehicles, 'r') as db:
+        vehicles = db.readlines()
+        
+        #Strip away extra whitespace 
+        vehicles = [vehicle.strip() for vehicle in vehicles]
+        
+        # Inform the user of authorized vehicles
+        print('    The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:')
+        print()
+        
+        # Print each vehicle on a separate line
+        for vehicle in vehicles:
+            print(f'      {vehicle}')
+        
+        # Close File
+        db.close()
 
 def print_searched_vehicles(authorizedVehicles):
     
@@ -70,19 +71,24 @@ def print_searched_vehicles(authorizedVehicles):
     search = input('Search for this vehicle: ')
     print ()
     
-    # read vehicles from file and search for input vehicle
+    # read vehicles from file
     import os
-    if os.path.isfile(authorizedVehicles):
-        with open(authorizedVehicles, 'r') as db:
-            vehicles = db.readlines()
-            vehicles = [vehicle.strip() for vehicle in vehicles]
-    
-    # Check and print results for matching vehicles
-    if search in vehicles:
-        print(f'{search} is an authorized vehicle.')
-    else:
-        print(f'{search} is not an authorized vehicle, if you received this in error please check the spelling and try again.')
 
+    with open(authorizedVehicles, 'r') as db:
+        vehicles = db.readlines()
+        vehicles = [vehicle.strip() for vehicle in vehicles]
+        
+        # Check and print results for no matching vehicles
+        if search not in vehicles:
+            print(f'{search} is not an authorized vehicle, if you received this in error please check the spelling and try again.')
+            
+        # Print results for matching vehicles
+        else:
+            print(f'{search} is an authorized vehicle.')
+        
+        # Close file
+        db.close
+        
 def append_authorized_vehicles_file (authorizedVehicles):
     
     ## Allow user to add vehicles to file
@@ -98,89 +104,132 @@ def append_authorized_vehicles_file (authorizedVehicles):
             break
         print ("Input not entered.  Please try again.")
     
-    # Read vehicles in file
-    with open(authorizedVehicles, "r") as db:
+    # Open Vehicle File with read/write permission
+    with open(authorizedVehicles, "r+") as db:
         vehicles = db.readlines()
         vehicles = [vehicle.strip() for vehicle in vehicles]
     
-    # Check if vehicles is in list before adding
-    if update not in vehicles:
-        with open (authorizedVehicles, 'a') as db:
+        # Check if vehicles is in list before adding
+        if update not in vehicles:
             db.write (update + "\n")
-            db.close
-        print (f'"{update}" has been added to the Authorized Vehicles List.')
-        print(f'\033[1mYou have added "{update}" as an authoriezed vehicle.\033[0m')
-    else:
-        print (f'"{update}" is already in the Authorized Vehicles List')
+            print (f'"{update}" has been added to the Authorized Vehicles List.')
+            print(f'\033[1mYou have added "{update}" as an authorized vehicle.\033[0m') 
+        
+       # Notify user vehicle to add is already in file 
+        else:
+            print (f'"{update}" is already in the Authorized Vehicles List')
+        db.close()
 
 def remove_authorized_vehicles_file (authorizedVehicles):
     
-    ## Allow user to add vehicles to file
-    ## Prevents duplicate entries
+    ## Allow user to remove vehicles to file
     
     print('\033[1mPlease ENTER the full Vehicle name you would like to REMOVE:\033[0m')
     print()
     
-    # Get user input to update list
+    # Request user input
     while True:
         removal = input('Update to remove:  ')
+        
+        # check if user did not enter value
         if removal:
             break
         print ("Input not entered.  Please try again.")
 
     # Read vehicles in file
-    with open(authorizedVehicles, "r") as db:
+    with open(authorizedVehicles, "r+") as db:
         vehicles = db.readlines()
         vehicles = [vehicle.strip() for vehicle in vehicles]
+
+        # Check if requested vehicle is listed in file
+        if removal not in vehicles:
+            
+            # Notify user if vehicle is not in file
+            print(f'"{removal}" is not in the Authorized Vehicle List.')
+            return    
+
+        # If vehicle in file, confirm request to remove before removing
+        else:
+            while True:
+                confirm = input(f'** Are you sure you want to remove {removal} from the Authorized Vehicles List?  ')
     
-    # Check if vehicles is in list before removing
-    if removal in vehicles:
-
-        #Ask for confirmation to remove
-        while True:
-            confirm = input(f'** Are you sure you want to remove {removal} from the Authorized Vehicles List?  ')
-            if confirm.lower() == 'yes':
-
-                # Remove vehicle from the list
+                # Display error if value other than 'yes' or 'no' has been entered
+                if confirm.lower() not in ['yes', 'no']:
+                    print(f'{confirm} is Invalid response. Please enter "yes" or "no".')
+                
+                # Repeat confirmation loop if invalid value was entered
+                else:
+                    break                
+                
+            # Return to main menu if user responds 'no'
+            if confirm.lower() == 'no':
+                print("Vehicle removal cancelled.")
+                return
+                
+            # Remove vehicle from the list if user responds 'yes'
+            elif confirm.lower() == 'yes':
                 vehicles.remove(removal)
 
-                with open (authorizedVehicles, 'w') as db:
-                    for vehicle in vehicles:
-                        db.write (vehicle + "\n")
+                # Clear list before updating
+                db.seek(0)
+                db.truncate()
+
+                # Write the updated list to the file
+                for vehicle in vehicles:
+                    db.write (vehicle + "\n")
+
+                # Display confirmation messages    
                 print (f'"{removal}" has been deleted from the Authorized Vehicles List.')
-                print(f'\033[1mYou have removed "{removal}" as an authoriezed vehicle.\033[0m')
-                break
-            elif confirm.lower() == 'no':
-                print("Vehicle removal cancelled.")
-                break
-            elif confirm == "":
-                print("No response entered.  Please confirm deletion by entering 'yes' or 'no'. ")
-            else:
-                print ('Invalid response. Please enter "yes" or "no".')
-    else:
-        print(f'"{removal}" is not in the Authorized Vehicle List.')
+                print(f'\033[1mYou have removed "{removal}" as an authorized vehicle.\033[0m')
+        
+        # Close File
+        db.close
 
 def reset_authorized_vehicles_file (authorizedVehicles):
     
     ## Reset file to the predefined list
-    ## Overwrites any changes made to file
+    ## Overwrites any previous changes made to file
+
+    # Display warning
+    print(f'\033[1mYou are attempting to {menuOptions[4]}.\033[0m')
+    print('\033[1mThis will restore the the Authorized Vehicle List to original.\033[0m')
+    print('\033[1mAny changes previously made will be lost.\033[0m')
+    print()
+
+    # Request user confrimation before continuing
+    reset = input('Are you sure you wish to proceed? (Yes/No): ').strip().lower()
+
+    # Proceed with reset
+    if reset == 'yes':
+
+        # open vehicle list file with write permissions
+        with open(authorizedVehicles, 'w') as db:
+                
+            # Overwrite current list and replace with list in "allowedVehiclesList"
+            for vehicle in allowedVehiclesList:
+                
+                # Write each vehicle on a separate line with a new line for future entries
+                db.write(vehicle + "\n")
+                
+            # Close file
+            db.close
+        print (f'"Authorized Vehicles List has been restored to original.')
     
-    import os
-    if os.path.isfile(authorizedVehicles):
-    
-        # read file contents
-        with open(authorizedVehicles, 'r') as db:
-            vehicles = db.readlines()
-            vehicles = [vehicle.strip() for vehicle in vehicles]
-    
-        # only reset if file does not match predefined list
-        if vehicles == allowedVehiclesList:
-            return
-    
-    # reset file
-    with open(authorizedVehicles, 'w') as db:
-        for vehicle in allowedVehiclesList:
-            db.write(vehicle + "\n")
+    else:
+        print(f"{menuOptions[4]} cancelled.")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
 
@@ -240,6 +289,11 @@ def main():
                 # Option 4: delete authorized vehicles
                 elif response == 4:
                     remove_authorized_vehicles_file(authorizedVehicles)
+                
+                # Option 5: Clear all changes and reset default vehicle list
+                elif response == 5:
+                    reset_authorized_vehicles_file (authorizedVehicles)
+
             
             # Handle case where the user selects an unavailable option
             else:
